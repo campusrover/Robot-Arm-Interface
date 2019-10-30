6dfaeac1f9d4a414b5706ce2ccc27f8f7a30bc95
Jacob Smith <jsmith2021@brandeis.edu>
2019-10-29 20:54:42 -0400

    Added Example Files to get the Arduino to talk to the Pi, updated README

---
+++ extras/ArmPiV1.py
@@ -0,0 +1,11 @@
+import serial
+import RPi.GPIO as GPIO
+import time
+
+ser=serial.Serial("/dev/ttyUSB0",9600)
+ser.baudrate=9600
+
+while True:
+    read_ser=ser.readline()
+    print(read_ser)
+    
