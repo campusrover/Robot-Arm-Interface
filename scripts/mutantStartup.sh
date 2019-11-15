0b23efffb4044e25a17a911933847f525b49f06e
Jacob Smith <jsmith2021@brandeis.edu>
2019-11-15 09:20:25 -0500

    changed usb port

--- scripts/mutantStartup.sh
+++ scripts/mutantStartup.sh
@@ -6,12 +6,13 @@ export ROS_MASTER_URI=http://mutant.dyn.brandeis.edu:11311/
 source /opt/ros/kinetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 export DISPLAY=:0.0
-
+sleep 5
 
 echo Starting Roscore and Bringup
 #gnome-terminal -e "roscore & roslaunch --wait turtlebot3_bringup turtlebot3_robot.launch"
 gnome-terminal -e 'sh -c "roscore & roslaunch --wait turtlebot3_bringup turtlebot3_robot.launch"'
-sleep 5
+echo Please Unplug and Replug USB on top of robot which has Rubber Band
+sleep 30
 echo Updating Script on Rasberry Pi
 sourceDir=~/catkin_ws/src/Robot-Arm-Interface/
 fileName=scripts/topic_subscriber.py
