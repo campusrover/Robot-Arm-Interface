3dffd977b97af37a2802dcfbaef5602dfaa5f10b
Jacob Smith <jsmith2021@brandeis.edu>
2019-11-15 06:48:03 -0500

    export ROS_MASTER_URI=http://mutant.dyn.brandeis.edu:11311/

--- scripts/mutantStartup.sh
+++ scripts/mutantStartup.sh
@@ -1,5 +1,6 @@
-#run with `ssh mutant@mutant.dyn.brandeis.edu 'bash' < '/home/robot/catkin_ws/src/arminterface/scripts/mutantStartup.sh'`
+#run with ssh mutant@mutant.dyn.brandeis.edu 'bash' < '/home/robot/catkin_ws/src/arminterface/scripts/mutantStartup.sh'
 export ROS_MASTER_URI=http://mutant.dyn.brandeis.edu:11311/
 source /opt/ros/kinetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 roscore & roslaunch --wait turtlebot3_bringup turtlebot3_robot.launch
+#ssh mutant@mutant.dyn.brandeis.edu 'python' < '/home/robot/catkin_ws/src/arminterface/scripts/topic_subscriber.py'
--- scripts/topic_subscriber.py
+++ scripts/topic_subscriber.py
@@ -25,7 +25,7 @@ def sendCommand (characterCommand):
 		print ("\tElement did not respond in time")
 	print("")
 try:
-	ser=serial.Serial("/dev/ttyUSB2",9600,timeout=5)	
+	ser=serial.Serial("/dev/ttyUSB1",9600,timeout=5)	
 	ser.baudrate=9600
 	print("Starting Serial Connection")
 
