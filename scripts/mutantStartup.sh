f0aed3a86a695dbecf29fd9c2be98d76d043a9fa
Jacob Smith <jsmith2021@brandeis.edu>
2019-11-15 06:58:40 -0500

    editing startup script to automatically update git repo on pi, and making usb port wrong on purpose to test updating

--- scripts/mutantStartup.sh
+++ scripts/mutantStartup.sh
@@ -2,5 +2,11 @@
 export ROS_MASTER_URI=http://mutant.dyn.brandeis.edu:11311/
 source /opt/ros/kinetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
-roscore & roslaunch --wait turtlebot3_bringup turtlebot3_robot.launch
+roscore & roslaunch --wait turtlebot3_bringup turtlebot3_robot.launch 
+#make sure local program is updated
+sourceDir=~/catkin_ws/src/Robot-Arm-Interface/
+fileName=scripts/topic_subscriber.py
+cd sourceDir
+git pull
+python $sourceDir$fileName
 #ssh mutant@mutant.dyn.brandeis.edu 'python' < '/home/robot/catkin_ws/src/arminterface/scripts/topic_subscriber.py'
