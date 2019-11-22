702cf6fca682496856e899fa35df0151f390ca32
Jacob Smith <jsmith2021@brandeis.edu>
2019-11-21 23:25:38 -0500

    added messaeg to se if auto update wprls

--- scripts/runRos.sh
+++ scripts/runRos.sh
@@ -1,6 +1,9 @@
  #!/bin/bash
 #Jacob smith COSI 119A 11/7/2019 ASrm Interface
 #run publisher node with a delay so robot can start up
+echo update worked!
+sleep 5
+
 gnome-terminal -e 'sh -c "sleep 40;cd ~/catkin_ws/src/arminterface/scripts;python topic_publisher.py"'
 #start robot and run subscriber node
 ssh mutant@mutant.dyn.brandeis.edu 'bash' < '/home/robot/catkin_ws/src/arminterface/scripts/mutantStartup.sh'
